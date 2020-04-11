98b5353d1facc3b30e806a922f85f56724e647fe
williamzhangsjtu <57250515+williamzhangsjtu@users.noreply.github.com>
2020-04-11 22:16:56 +0800

    Add files via upload

--- leetcode/127_Word Ladder/main.cpp
+++ leetcode/127_Word Ladder/main.cpp
@@ -47,43 +47,86 @@ using namespace std;
 // };
 
 
-class Solution {
-public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
-    {
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        queue <string> Q;
-        Q.push(beginWord);
-        string tmp;
-        int index, current_height = 1, layer_num = 1, next_num = 0;
-        while(!Q.empty())
-        {
-            tmp = Q.front();
-            Q.pop();
-            if (tmp == endWord) return current_height;
-            for (int i = 0; i < tmp.size(); i++)
-            {
-                char ch = tmp[i];
-                for (int j = 0; j < 26; ++j)
-                {
-                    tmp[i] = 'a' + j;
-                    if (dict.find(tmp) != dict.end()) 
-                    {
-                        Q.push(tmp);
-                        dict.erase(tmp);
-                        next_num++;
-                    }
-                }
-                tmp[i] = ch;
-            }
-            if (!--layer_num) 
-            {
-                layer_num = next_num;
-                current_height++;
-                next_num = 0;
-            }
+// class Solution {
+// public:
+//     int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
+//     {
+//         unordered_set<string> dict(wordList.begin(), wordList.end());
+//         queue <string> Q;
+//         Q.push(beginWord);
+//         string tmp;
+//         int index, current_height = 1, layer_num = 1, next_num = 0;
+//         while(!Q.empty())
+//         {
+//             tmp = Q.front();
+//             Q.pop();
+//             if (tmp == endWord) return current_height;
+//             for (int i = 0; i < tmp.size(); i++)
+//             {
+//                 char ch = tmp[i];
+//                 for (int j = 0; j < 26; ++j)
+//                 {
+//                     tmp[i] = 'a' + j;
+//                     if (dict.find(tmp) != dict.end()) 
+//                     {
+//                         Q.push(tmp);
+//                         dict.erase(tmp);
+//                         next_num++;
+//                     }
+//                 }
+//                 tmp[i] = ch;
+//             }
+//             if (!--layer_num) 
+//             {
+//                 layer_num = next_num;
+//                 current_height++;
+//                 next_num = 0;
+//             }
                 
 
+//         }
+//         return 0;
+//     }
+// };
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end()), head, tail, *phead, *ptail;
+        if (dict.find(endWord) == dict.end()) {
+            return 0;
+        }
+        head.insert(beginWord);
+        tail.insert(endWord);
+        int ladder = 2;
+        while (!head.empty() && !tail.empty()) {
+            if (head.size() < tail.size()) {
+                phead = &head;
+                ptail = &tail;
+            } else {
+                phead = &tail;
+                ptail = &head;
+            }
+            unordered_set<string> temp;
+            for (auto it = phead -> begin(); it != phead -> end(); it++) {    
+                string word = *it;
+                for (int i = 0; i < word.size(); i++) {
+                    char t = word[i];
+                    for (int j = 0; j < 26; j++) {
+                        word[i] = 'a' + j;
+                        if (ptail -> find(word) != ptail -> end()) {
+                            return ladder;
+                        }
+                        if (dict.find(word) != dict.end()) {
+                            temp.insert(word);
+                            dict.erase(word);
+                        }
+                    }
+                    word[i] = t;
+                }
+            }
+            ladder++;
+            phead -> swap(temp);
         }
         return 0;
     }
